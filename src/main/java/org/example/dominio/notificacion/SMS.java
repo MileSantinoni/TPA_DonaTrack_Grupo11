5caae4ab4f857cf40b766e259f4adba3d207fd5b
Gianluca Carone <gcaronetonello@frba.utn.edu.ar>
2026-07-09 13:44:47 -0300

    envio WPP y SMS real

--- src/main/java/org/example/dominio/notificacion/SMS.java
+++ src/main/java/org/example/dominio/notificacion/SMS.java
@@ -1,18 +1,30 @@
 package org.example.dominio.notificacion;
 
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SMS implements TipoNotificacion {
+
+  @Value("${twilio.smsFrom}")
+  private String remitente;
 
   @Override
   public Notificacion enviar(String destinatario, String mensaje) {
     Notificacion notificacion = new Notificacion(destinatario, mensaje);
 
     try {
-      System.out.println("[SMS simulado] Para: " + destinatario + " | Mensaje: " + mensaje);
+      Message.creator(
+          new PhoneNumber(destinatario),
+          new PhoneNumber(remitente),
+          mensaje
+      ).create();
+
       notificacion.marcarComoCompletada();
     } catch (Exception e) {
+      e.printStackTrace();
       notificacion.marcarComoFallida();
     }
 
